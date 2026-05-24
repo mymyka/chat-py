9d656ec215eddb87719b1795f0e3d8a13b33c26b
Illia <kasianenkoilia@gmail.com>
2026-05-24 21:16:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,46 @@
 import socket
 import threading
+from textual.app import App, ComposeResult
+from textual.widgets import Input, RichLog
+from textual.containers import Vertical
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost",8080))
+client.connect(("192.168.1.13", 8080))
 
 
-# while True:
-#     user_input = input("You: ")
-#     client.send(user_input.encode())
-#     print("##:: " + client.recv(1024).decode())
+class ChatApp(App):
+
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            RichLog(id="log"),
+            Input(placeholder="Type message...", id="input"),
+        )
+
+    def on_mount(self):
+        threading.Thread(target=self.receive_loop, daemon=True).start()
+
+    def receive_loop(self):
+        while True:
+            try:
+                msg = client.recv(1024).decode()
+                if msg:
+                    self.call_from_thread(
+                        self.query_one("#log", RichLog).write,
+                        f"Server: {msg}"
+                    )
+            except:
+                break
+
+    def on_input_submitted(self, event: Input.Submitted):
+        msg = event.value.strip()
+        if not msg:
+            return
+
+        client.send(msg.encode())
+
+        self.query_one("#log", RichLog).write(f"You: {msg}")
+        self.query_one("#input", Input).value = ""
 
 
-def receving():
-    while True:
-        try:
-            msg =  client.recv(1024).decode()
-            if not msg:
-                continue
-            if msg:
-                print(f"##:",msg)
-        except:
-            break
-
-def main():
-      while True:
-        try:
-            msg = input("You: ")
-            if msg.strip() == "":
-                continue
-            client.send(msg.encode())
-        except:
-            break
-
-
-threading.Thread(target=receving, daemon=True).start()
-main()
+if __name__ == "__main__":
+    ChatApp().run()
