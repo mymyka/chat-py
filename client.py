86b4cb632c9454a336a4e0517837a1be15f28b98
Illia <kasianenkoilia@gmail.com>
2026-05-24 20:28:23 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+client = socket.socket(socket.AF_INIET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1",9999))
+
+
+while True:
+    user_input = input("You: ")
+    client.send(user_input.encode())
+    print(client.recv(1024))
+
+
