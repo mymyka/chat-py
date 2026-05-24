208544065b07018dfbe26e04e660d0d8305f2254
Illia <kasianenkoilia@gmail.com>
2026-05-24 21:06:48 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,31 @@
 import socket
-import string
+import threading
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost",8080))
+client.connect(("192.168.1.13",8080))
 
 
-while True:
+# while True:
+#     user_input = input("You: ")
+#     client.send(user_input.encode())
+#     print("##:: " + client.recv(1024).decode())
+
+
+def receving():
+    while True:
+        try:
+            msg =  client.recv(1024).decode()
+            if not msg:
+                continue
+            if msg:
+                print(f"##:",msg)
+        except:
+            break
+
+def main():
     user_input = input("You: ")
     client.send(user_input.encode())
-    print(client.recv(1024).decode())
 
 
+threading.Thread(target=receving, daemon=True).start()
+main()
