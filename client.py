dce7b3afc5158f1ff403b49fa580b67c7808309e
The Future Pirate King <kasianenkomykyta@gmail.com>
2026-05-24 20:51:48 +0200

    a

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
-
+import string
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("192.168.1.13",8080))
+client.connect(("localhost",8080))
 
 
 while True:
     user_input = input("You: ")
     client.send(user_input.encode())
-    print(client.recv(1024).encode)
+    print(client.recv(1024).decode())
 
 
