af345e44cbc3f444c5e4fcfdfaffb114de0fd26e
Illia <kasianenkoilia@gmail.com>
2026-05-24 20:49:13 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1",8080))
+client.connect(("192.168.1.13",8080))
 
 
 while True:
     user_input = input("You: ")
     client.send(user_input.encode())
-    print(client.recv(1024))
+    print(client.recv(1024).encode)
 
 
