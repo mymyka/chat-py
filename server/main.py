8e82aa362d717c28b4524b0c95c911b3b17be5a0
The Future Pirate King <kasianenkomykyta@gmail.com>
2026-05-24 20:35:20 +0200

    a

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 8080))
@@ -7,6 +8,7 @@ server.listen(5)
 
 state = {}
 
-while True:
-    client, addr = server.accept()
-    state[addr] = client
+def state_manager():
+    while True:
+        client, addr = server.accept()
+        state[addr] = client
